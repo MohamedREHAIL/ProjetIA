11e98ead2c7877d1cb1e993f13d88d803bea0419
TalkBog <99033517+TalkBog@users.noreply.github.com>
2024-01-09 15:48:39 +0100

    Update IA.py

--- IA.py
+++ IA.py
@@ -7,9 +7,10 @@ import pandas as pd
 import pickle
 
 test_size_score = 0
+max_accuracy = 0
 
 def moyenne (test_size, random_state):
-    global test_size_score
+    global test_size_score, max_accuracy
     if test_size == 1.0 :
         return
     
@@ -24,18 +25,13 @@ def moyenne (test_size, random_state):
     regressor.fit(X_train, y_train)
 
     y_pred = regressor.predict(X_test)
-
-
     accuracy = accuracy_score(y_test, y_pred)
 
-    print(accuracy)
-
-
-    if accuracy <= test_size_score :
+    if accuracy <= max_accuracy :
         moyenne(test_size+0.1,0)
     else:
         test_size_score = test_size
-        print(accuracy, test_size)
+        max_accuracy = accuracy
         with open('model.pkl', 'wb') as f:
             pickle.dump(regressor, f)
 

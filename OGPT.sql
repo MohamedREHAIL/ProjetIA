0351c1d558d74c8d617f386d0e9dfa4d732cb2f2
TalkBog <99033517+TalkBog@users.noreply.github.com>
2024-01-11 22:53:36 +0100

    Update OGPT.sql

--- OGPT.sql
+++ OGPT.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : db
--- Généré le : mer. 10 jan. 2024 à 22:28
+-- Généré le : jeu. 11 jan. 2024 à 21:50
 -- Version du serveur : 8.2.0
 -- Version de PHP : 8.2.14
 
@@ -33,8 +33,8 @@ CREATE TABLE `Form` (
   `nbEntreprise` int NOT NULL,
   `nbReponse` int NOT NULL,
   `nbEntretien` int NOT NULL,
-  `validate` BOOLEAN DEFAULT 0
-);
+  `validate` tinyint(1) DEFAULT '2'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -49,7 +49,7 @@ CREATE TABLE `Grade` (
   `Systeme` int NOT NULL,
   `BDD` int NOT NULL,
   `Stage` int DEFAULT NULL
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -64,9 +64,15 @@ CREATE TABLE `User` (
   `LastName` varchar(100) NOT NULL,
   `username` varchar(10) NOT NULL,
   `password` varchar(200) NOT NULL,
-  `UserList_id` int NOT NULL
+  `UserList_id` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-);
+--
+-- Déchargement des données de la table `User`
+--
+
+INSERT INTO `User` (`id`, `Type`, `FirstName`, `LastName`, `username`, `password`, `UserList_id`) VALUES
+(13, 'Admin', 'Admin', 'Admin', 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', NULL);
 
 -- --------------------------------------------------------
 
@@ -77,8 +83,8 @@ CREATE TABLE `User` (
 CREATE TABLE `UserList` (
   `id` int NOT NULL,
   `promo` enum('B1','B2') NOT NULL,
-  `name` int NOT NULL
-);
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Index pour les tables déchargées
@@ -89,6 +95,7 @@ CREATE TABLE `UserList` (
 --
 ALTER TABLE `Form`
   ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `userId` (`userId`),
   ADD KEY `Form_FK_1` (`userId`);
 
 --
@@ -96,6 +103,7 @@ ALTER TABLE `Form`
 --
 ALTER TABLE `Grade`
   ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `userId` (`userId`),
   ADD KEY `grade_FK_1` (`userId`);
 
 --
@@ -103,6 +111,7 @@ ALTER TABLE `Grade`
 --
 ALTER TABLE `User`
   ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
   ADD KEY `User_FK_1` (`UserList_id`);
 
 --
@@ -131,13 +140,13 @@ ALTER TABLE `Grade`
 -- AUTO_INCREMENT pour la table `User`
 --
 ALTER TABLE `User`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT pour la table `UserList`
 --
 ALTER TABLE `UserList`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Contraintes pour les tables déchargées
@@ -160,10 +169,6 @@ ALTER TABLE `Grade`
 --
 ALTER TABLE `User`
   ADD CONSTRAINT `User_FK_1` FOREIGN KEY (`UserList_id`) REFERENCES `UserList` (`id`);
-
-
-
-
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
